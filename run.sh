322ffe357e29bbcaa335b82db63179f88df83ad5
Alberto Miranda <amiranda@elementum.com>
2017-02-24 15:47:21 -0300

    updated docker tag

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [[ "$(docker images -q myimage:mytag 2> /dev/null)" == "" ]]; then
-docker build -t amiranda/lunchnotifier .
+docker build -t amiranda/lunch-notifier .
   # do something
 fi
 sleep 1
-docker run -p 80:9200 -d amiranda/lunchnotifier
+docker run -p 80:9200 -d amiranda/lunch-notifier
