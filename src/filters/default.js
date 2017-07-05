d044a25d074380db178609d474e329dbfca2f710
Alberto Miranda <amiranda@elementum.com>
2017-07-04 22:28:25 -0300

    Updated default date formatting; returning earlier when data is found.

--- src/filters/default.js
+++ src/filters/default.js
@@ -1,5 +1,5 @@
 const dateFormat = require('dateformat')
-const dateFormatting = process.env.DATE_FORMATTING || 'mmm d'
+const dateFormatting = process.env.DATE_FORMATTING || 'd-mmm'
 let key
 
 // Set rows filter.
@@ -15,6 +15,7 @@ function run (rows) {
     // Get col for current data.
     if (indexRows === 0) {
       row.some((value, indexRow) => {
+        // console.log(`CURRENT DATE: ${currentDate.toLowerCase()} / DATE IN ROW: ${value.toLowerCase()}`)
         if (value.toLowerCase() === currentDate.toLowerCase()) {
           currentDateCol = indexRow
           return true
@@ -25,11 +26,14 @@ function run (rows) {
 
     // Iterate row values.
     if (!row[0]) return false
+    // console.log(`CURRENT NAME: ${row[0]}`)
     if (row[0].toLowerCase() !== key.toLowerCase()) return false
 
     // Add lunch data for current date for selected person.
+    // console.log(`==== CURRENT DATE COL: ${currentDateCol}:`, row[currentDateCol])
     const lunch = row[currentDateCol] || `Oops, there's no lunch for you today buddy!`
     filteredRows.push(lunch)
+    return true
   })
 
   if (!filteredRows) {
